d10b6707835b684738f5c2700f6c84fb1c215f17
Adam <selmeczi-toth.adam@heller.uni-corvinus.hu>
2021-10-08 00:42:13 +0200

    market depth + visualizer

---
+++ tools/market_depth.py
@@ -0,0 +1,41 @@
+from os import listdir
+import pandas as pd
+
+
+markets = ["binance", "bitfinex", "bitstamp", "coinbase", "kraken"]
+main_path = "C:/Users/Adam/Documents/bitcoin-orderbook/order_books"
+
+markets_dict = {}
+
+for market in markets:
+    path = main_path + "/" + market
+    filenames = listdir(path)
+    files = [filename for filename in filenames if filename.endswith(".csv")]
+    for file in files:
+        # data = dask.dataframe.read_csv(path + "/" + file)
+        data = pd.read_csv(path + "/" + file)
+        timestamp = file.replace(market + "_", "").replace(".csv", "")
+        data["timestamp"] = timestamp
+        markets_dict.setdefault(market, [])
+        markets_dict[market].append(data)
+
+market_depth = {}
+for market in markets:
+    row_list = []
+    for element in markets_dict[market]:
+        lowest_bid = element.loc[element["type"] == "buy"]["price"].min()
+        highest_ask = element.loc[element["type"] == "sell"]["price"].max()
+        bid_amount = element.loc[element["price"] == lowest_bid]["total"].iloc[0]
+        ask_amount = element.loc[element["price"] == highest_ask]["total"].iloc[0]
+        timestamp = element["timestamp"][0]
+        row_list.append((timestamp, bid_amount, ask_amount, market))
+    market_depth[market] = pd.DataFrame(row_list, columns=("timestamp", "bid_amount", "ask_amount", "market"))
+
+for market in market_depth:
+    save_path = main_path + "/market_depth/"
+    market_depth[market].to_csv(save_path + market + ".csv", index=False)
+
+
+
+
+
---
+++ tools/market_depth_visualizer.py
@@ -0,0 +1,33 @@
+from os import listdir
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+import seaborn
+
+
+markets = ["binance", "bitfinex", "bitstamp", "coinbase", "kraken"]
+main_path = "C:/Users/Adam/Documents/bitcoin-orderbook/order_books/market_depth"
+
+market_depth = pd.DataFrame()
+
+for market in listdir(main_path):
+    path = main_path + "/" + market
+    data = pd.read_csv(path)
+    market_depth = market_depth.append(data, ignore_index=True)
+
+plt.rcParams["figure.figsize"] = (15,10)
+
+for market in markets:
+    plt.plot(pd.to_datetime(market_depth.loc[market_depth["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+             market_depth.loc[market_depth["market"] == market]["bid_amount"],
+             label = market.capitalize())
+
+# plt.style.use('stylename')
+plt.title('Market depth across exchanges (sum of bid and ask volume 5%)')
+
+plt.ylabel("Depth")
+plt.legend()
+
+# plt.show()
+plt.savefig('market_depth.png', dpi=300)
