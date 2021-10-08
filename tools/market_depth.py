6297a579bc165018ab3ad4cf984b2e178199f143
Adam <selmeczi-toth.adam@heller.uni-corvinus.hu>
2021-10-08 16:23:24 +0200

    new plots

--- tools/market_depth.py
+++ tools/market_depth.py
@@ -20,23 +20,35 @@ for market in markets:
         markets_dict[market].append(data)
 
 # Calculating market depth for each exchange at each timestamp
-market_depth = {}
+# market_depth = {}
+# for market in markets:
+#     row_list = []
+#     for element in markets_dict[market]:
+#         lowest_bid = element.loc[element["type"] == "buy"]["price"].min()
+#         highest_ask = element.loc[element["type"] == "sell"]["price"].max()
+#         bid_amount = element.loc[element["price"] == lowest_bid]["total"].iloc[0]
+#         ask_amount = element.loc[element["price"] == highest_ask]["total"].iloc[0]
+#         timestamp = element["timestamp"][0]
+#         row_list.append((timestamp, bid_amount, ask_amount, market))
+#     market_depth[market] = pd.DataFrame(row_list, columns=("timestamp", "bid_amount", "ask_amount", "market"))
+#
+# for market in market_depth:
+#     save_path = main_path + "/market_depth/"
+#     market_depth[market].to_csv(save_path + market + ".csv", index=False)
+
+
+# Calculating market spread for each exchange at each timestamp
+market_spread = {}
 for market in markets:
     row_list = []
     for element in markets_dict[market]:
-        lowest_bid = element.loc[element["type"] == "buy"]["price"].min()
-        highest_ask = element.loc[element["type"] == "sell"]["price"].max()
-        bid_amount = element.loc[element["price"] == lowest_bid]["total"].iloc[0]
-        ask_amount = element.loc[element["price"] == highest_ask]["total"].iloc[0]
+        highest_bid = element.loc[element["type"] == "buy"]["price"].max()
+        lowest_ask = element.loc[element["type"] == "sell"]["price"].min()
         timestamp = element["timestamp"][0]
-        row_list.append((timestamp, bid_amount, ask_amount, market))
-    market_depth[market] = pd.DataFrame(row_list, columns=("timestamp", "bid_amount", "ask_amount", "market"))
+        row_list.append((timestamp, highest_bid, lowest_ask, market))
+    market_spread[market] = pd.DataFrame(row_list, columns=("timestamp", "highest_bid", "lowest_ask", "market"))
 
-for market in market_depth:
-    save_path = main_path + "/market_depth/"
-    market_depth[market].to_csv(save_path + market + ".csv", index=False)
-
-
-
-
+for market in market_spread:
+    save_path = main_path + "/market_spread/"
+    market_spread[market].to_csv(save_path + market + ".csv", index=False)
 
--- tools/market_depth_visualizer.py
+++ tools/market_depth_visualizer.py
@@ -1,6 +1,7 @@
 from os import listdir
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from datetime import datetime
 import numpy as np
 import seaborn
@@ -16,15 +17,83 @@ for market in listdir(main_path):
     data = pd.read_csv(path)
     market_depth = market_depth.append(data, ignore_index=True)
 
+# Market depth across exchanges
+
+# plt.rcParams["figure.figsize"] = (15,10)
+# for market in markets:
+#     plt.plot(pd.to_datetime(market_depth.loc[market_depth["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+#              market_depth.loc[market_depth["market"] == market]["bid_amount"] +
+#              market_depth.loc[market_depth["market"] == market]["ask_amount"],
+#              label = market.capitalize())
+#
+# plt.title('Market depth across exchanges (sum of bid and ask volume 5%)')
+# plt.ylabel("Depth")
+# plt.legend()
+# plt.savefig('market_depth.png', dpi=300)
+
+
+# Bid-ask differences across exchanges
+
+# plt.rcParams["figure.figsize"] = (15,15)
+# fig, axs = plt.subplots(5)
+# for item, ax in enumerate(axs):
+#     market = markets[item]
+#     ax.plot(pd.to_datetime(market_depth.loc[market_depth["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+#              market_depth.loc[market_depth["market"] == market]["bid_amount"],
+#             label="Bid volume")
+#     ax.plot(pd.to_datetime(market_depth.loc[market_depth["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+#              market_depth.loc[market_depth["market"] == market]["ask_amount"],
+#             label="Ask volume")
+#     ax.set_title(market.capitalize())
+#     if item == 0:
+#         box = ax.get_position()
+#         ax.set_position([box.x0, box.y0 + box.height * 0.1,
+#                          box.width, box.height * 0.9])
+#         ax.legend(loc='upper right', bbox_to_anchor=(1, 1.2),
+#                   fancybox=True, shadow=True, ncol=5)
+#
+# for ax in axs.flat:
+#     ax.label_outer()
+#
+# plt.tight_layout(pad=3.0)
+# plt.savefig('bid_ask_diff.png', dpi=300)
+
+
+# Bid-ask spread
+main_path = "C:/Users/Adam/Documents/bitcoin-orderbook/order_books/market_spread"
+
+market_spread = pd.DataFrame()
+
+for market in listdir(main_path):
+    path = main_path + "/" + market
+    data = pd.read_csv(path)
+    market_spread = market_spread.append(data, ignore_index=True)
+
+# plt.rcParams["figure.figsize"] = (15,10)
+# for market in markets:
+#     plt.plot(pd.to_datetime(market_spread.loc[market_spread["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+#              market_spread.loc[market_spread["market"] == market]["lowest_ask"] -
+#              market_spread.loc[market_spread["market"] == market]["highest_bid"],
+#              label = market.capitalize())
+# plt.title('Market spread across exchanges')
+# plt.ylabel("Spread")
+# plt.legend()
+# plt.savefig('market_spread.png', dpi=300)
+
+
+# Price differences
 plt.rcParams["figure.figsize"] = (15,10)
-
+base_currency = "kraken"
 for market in markets:
-    plt.plot(pd.to_datetime(market_depth.loc[market_depth["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
-             market_depth.loc[market_depth["market"] == market]["bid_amount"] +
-             market_depth.loc[market_depth["market"] == market]["ask_amount"],
+    plt.plot(pd.to_datetime(market_spread.loc[market_spread["market"] == market]["timestamp"], format="%Y.%m.%d.%H-%M"),
+             ((market_spread.loc[market_spread["market"] == market]["lowest_ask"] +
+               market_spread.loc[market_spread["market"] == market]["highest_bid"]) / 2).reset_index(drop=True) /
+             ((market_spread.loc[market_spread["market"] == base_currency]["lowest_ask"] +
+               market_spread.loc[market_spread["market"] == base_currency]["highest_bid"]) / 2).reset_index(drop=True),
              label = market.capitalize())
-
-plt.title('Market depth across exchanges (sum of bid and ask volume 5%)')
-plt.ylabel("Depth")
+plt.title("")
 plt.legend()
-plt.savefig('market_depth.png', dpi=300)
+plt.gca().set_yticklabels(['{:.0f}%'.format(x) for x in plt.gca().get_yticks()])
+plt.show()
+# plot_name = base_currency + "_price_diff.png"
+# plt.savefig(plot_name, dpi=300)
